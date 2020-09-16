f19d9f4df6046a254e1266f7b7f82a18efea1d88
Legonzaur <34353603+Legonzaur@users.noreply.github.com>
2020-09-16 20:20:32 +0200

    Update android-screen.sh
    adding configuration
    adding nvidia nvenc encoding

--- android-screen.sh
+++ android-screen.sh
@@ -1,27 +1,40 @@
-ipaddr="127.0.0.1:8888"
+###CONFIGURATION###
+
+#Initial IP address and port when no IP address is given
+IPADDR="127.0.0.1:8888"
+
+#Height of the virtual screen
+VIRTXRES=720
+
+#Width of the virtual screen
+VIRTYRES=1400
+
+#Height offset of the virtual screen
+OFFSETX=500
+
+#Width offset of the virtual screen
+OFFSETY=1920
+
+
+
+###SCRIPT###
 if [ -n "$1" ];
 then
-    ipaddr=$1
+    IPADDR=$1
 fi
 
-virtxres=720
-virtyres=1280
-Xmode="1280x720_60.00"
+Xmode=${VIRTYRES}x${VIRTXRES}_60.00
 
-$(xrandr --output HDMI-1 --mode $Xmode --pos 1920x500 &&
-ffmpeg -f x11grab -video_size ${virtyres}x${virtxres} -i :0.0+1920,500 -framerate 60 -tune:v fastdecode+zerolatency -preset:v ultrafast -crf 0 -vcodec libx264rgb -f mpegts "tcp://${ipaddr}?listen" 2> /dev/null ||
+xrandr --newmode $(gtf ${VIRTYRES} ${VIRTXRES} 60 | sed -ne 's/"//g;s/ Modeline //p')
+xrandr --addmode HDMI-1 "$Xmode"
+
+$(xrandr --output HDMI-1 --mode $Xmode --pos ${OFFSETY}x${OFFSETX} &&
+ffmpeg -probesize 32 -analyzeduration 0 -nostdin -hwaccel cuda -hwaccel_output_format cuda -framerate 60 -f x11grab -video_size ${VIRTYRES}x${VIRTXRES} -i :0.0+${OFFSETY},${OFFSETX} -framerate 60 -flags low_delay -fflags nobuffer+fastseek+flush_packets -strict experimental -tune:v fastdecode+zerolatency -analyzeduration 0 -threads auto -preset:v ultrafast -crf 0 -vcodec libx264rgb -f mpegts "tcp://${IPADDR}?listen" || #2> /dev/null ||
 xrandr --auto --output HDMI-1 --off) &
 
-
-sleep 1 &&
-adb reverse tcp:8888 tcp:8888 &&
-adb shell am start -d "tcp://${ipaddr}" -a android.intent.action.VIEW
-
-
-#MPV config for low latency : 
-#no-cache
-#untimed
-#no-demuxer-thread
-#video-sync=audio
-#vd-lavc-threads=1
-
+#if [ -a "$1" ]
+#then
+    sleep 1 &&
+    adb reverse tcp:8888 tcp:8888 &&
+    adb shell am start -d "tcp://${IPADDR}" -a android.intent.action.VIEW
+#fi
