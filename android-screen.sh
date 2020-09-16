01b5c1adc96dd52950a81bf7de99b89a9051c66a
Legonzaur <34353603+Legonzaur@users.noreply.github.com>
2020-09-16 13:02:25 +0200

    Update android-screen.sh

--- android-screen.sh
+++ android-screen.sh
@@ -12,13 +12,11 @@ $(xrandr --output HDMI-1 --mode $Xmode --pos 1920x500 &&
 ffmpeg -f x11grab -video_size ${virtyres}x${virtxres} -i :0.0+1920,500 -framerate 60 -tune:v fastdecode+zerolatency -preset:v ultrafast -crf 0 -vcodec libx264rgb -f mpegts "tcp://${ipaddr}?listen" 2> /dev/null ||
 xrandr --auto --output HDMI-1 --off) &
 
-if [ -n "$1" ]
-then
-else
-    sleep 1 &&
-    adb reverse tcp:8888 tcp:8888 &&
-    adb shell am start -d "tcp://${ipaddr}" -a android.intent.action.VIEW
-fi
+
+sleep 1 &&
+adb reverse tcp:8888 tcp:8888 &&
+adb shell am start -d "tcp://${ipaddr}" -a android.intent.action.VIEW
+
 
 #MPV config for low latency : 
 #no-cache
