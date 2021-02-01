6da44fa8bde35194d91c9f7d321cbcdf152356f6
centos <centos@centos.network>
2021-02-01 07:53:15 -0500

    removing GLPIdocker installation

--- Work/installGLPI-docker.sh
+++
@@ -1,4 +0,0 @@
-podman network create LAMP
-podman run --name mysql -e MYSQL_ROOT_PASSWORD=diouxx -e MYSQL_DATABASE=glpidb -e MYSQL_USER=glpi_user -e MYSQL_PASSWORD=glpi -d mysql:5.7.23 --network LAMP
-podman run --name glpi --publish 80:80 -d diouxx/glpi --network LAMP
-
