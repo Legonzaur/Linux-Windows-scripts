d69eae3dc7b2d4c4db7b7db1d31c9297b9b4bbf6
Legonzaur <34353603+Legonzaur@users.noreply.github.com>
2021-08-09 13:32:04 +0200

    Update ffmpeg_two_pass.sh

--- Video Encoding/ffmpeg_two_pass.sh
+++ Video Encoding/ffmpeg_two_pass.sh
@@ -2,16 +2,16 @@
 
 mkdir -p /tmp/ffmpeg_two_pass
 
-desired_size=90
-preset=slower
+desired_size=25
 
 duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $1)
 desired_size=$(($desired_size*8192))
 duration=$(perl -w -e "use POSIX; print ceil($duration/1.0), qq{\n}")
+preset=slower
 (( desired_rate=desired_size/duration ))
 desired_rate=${desired_rate}k
 
-ffmpeg -y -i $1 -c:v libx264 -preset $preset -b:v $desired_rate -pass 1 -passlogfile /tmp/ffmpeg_two_pass/$1 -vsync cfr -f null /dev/null && \
-ffmpeg -i $1 -c:v libx264 -preset $preset -b:v $desired_rate -pass 2 -passlogfile /tmp/ffmpeg_two_pass/$1 -c:a aac -b:a 128k ${1}-converted.mp4
+pv $1 | ffmpeg -y -i pipe:0 -v warning -c:v libx264 -preset $preset -b:v $desired_rate -pass 1 -passlogfile /tmp/ffmpeg_two_pass/$1 -vsync cfr -f null /dev/null && \
+pv $1 | ffmpeg -i pipe:0 -v warning -c:v libx264 -preset $preset -b:v $desired_rate -pass 2 -passlogfile /tmp/ffmpeg_two_pass/$1 -c:a aac -b:a 128k ${1}-converted.mp4
                                                                   
 rm /tmp/ffmpeg_two_pass/${1}-0.log*
