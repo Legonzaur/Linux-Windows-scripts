cc4b49e98ad02d945e7c3006dd13440a39455b56
Legonzaur <34353603+Legonzaur@users.noreply.github.com>
2023-05-25 13:26:05 +0200

    Add files via upload

---
+++ Video Encoding/Windows/25libvp9.cmd
@@ -0,0 +1,24 @@
+@echo off
+
+SET target_video_size_MB=23
+
+for /f %%i in ('ffprobe -v error -show_entries "format=duration" -of "default=noprint_wrappers=1:nokey=1" "%~1"') do set origin_duration_s=%%i
+
+for /f %%i in ('ffprobe -v error -select_streams a:0 -show_entries "stream=bit_rate" -of "default=noprint_wrappers=1:nokey=1" "%~1"') do set origin_audio_bitrate=%%i
+
+IF "%origin_audio_bitrate%"=="N/A" SET origin_audio_bitrate=134144
+IF NOT "%~2"=="" SET origin_audio_bitrate=%~2
+
+for /f %%i in ('powershell "[math]::Ceiling(%origin_duration_s%*%origin_audio_bitrate%)"') do set target_audio_size=%%i
+
+for /f %%i in ('powershell "[math]::Floor(((%target_video_size_MB%*8786432)-%target_audio_size%)/%origin_duration_s%)"') do set target_video_bitrate_kbit_s=%%i
+
+SET file=%~1
+IF "%~2"=="" SET origin_audio_bitrate=134144
+SET bv=%target_video_bitrate_kbit_s%
+SET output=%~1-25mb-libvpx-vp9
+SET args=-strict -2 -c:v libvpx-vp9 -cpu-used 4 -auto-alt-ref 1 -aq-mode 0 -tune-content screen -deadline good -lag-in-frames 25 -row-mt 1 -threads 8 
+
+ffmpeg -y -hwaccel auto -i "%file%" %args% -passlogfile "%output%" -b:v %bv% -pass 1 -an -f null NIL && ffmpeg -hwaccel auto -i "%file%" %args% -passlogfile "%output%" -b:v %bv% -pass 2 -c:a copy -map 0:v:0 -map 0:a:0 "%output%.mp4"
+del "%output%-0.log"
+del "%output%-0.log.mbtree"
