7bc90cfa0865044c44370c2d0b1c62f835874e9c
legonzaur <legonzaur@live.fr>
2024-02-23 11:09:09 +0100

    Update encoding scripts

--- Video Encoding/Linux/25nvenc720.sh
+++ Video Encoding/Linux/25nvenc720.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-target_video_size_MB=7
+target_video_size_MB=23
 file=$1
-origin_duration_s=$(ffprobe.exe -v error -show_streams -select_streams a "$file" | grep -Po "(?<=^duration\=)\d*\.\d*")
-origin_audio_bitrate_kbit_s=$(ffprobe.exe -v error -pretty -show_streams -select_streams a "$file" | grep -Po "(?<=^bit_rate\=)\d*\.\d*")
+origin_duration_s=$(ffprobe -v error -show_streams -select_streams a "$file" | grep -Po "(?<=^duration\=)\d*\.\d*")
+origin_audio_bitrate_kbit_s=$(ffprobe -v error -pretty -show_streams -select_streams a "$file" | grep -Po "(?<=^bit_rate\=)\d*\.\d*")
 target_audio_bitrate_kbit_s=128
 target_video_bitrate_kbit_s=$(\
     awk \
@@ -10,16 +10,19 @@ target_video_bitrate_kbit_s=$(\
     -v duration="$origin_duration_s" \
     -v audio_rate="$target_audio_bitrate_kbit_s" \
     'BEGIN { print  ( ( size * 8192.0 ) / ( 1.048576 * duration ) - audio_rate ) }')
-	ffmpeg.exe \
-    -i "$file" \
-	-preset:v p7\
+	ffmpeg \
+	-hwaccel cuda \
+	-hwaccel_output_format cuda \
+    	-i "$file" \
+	-preset:v slow \
 	-profile:v high \
 	-tune:v hq \
 	-2pass true \
 	-multipass fullres \
+	-vf scale_cuda=-2:720 \
     -c:v h264_nvenc \
     -b:v "$target_video_bitrate_kbit_s"k \
     -c:a aac \
     -b:a "$target_audio_bitrate_kbit_s"k \
-    "${file%.*}-${target_video_size_MB}mB-nvenc.mp4"
+    "${file%.*}-${target_video_size_MB}mB-nvenc720.mp4"
 	
--- Video Encoding/Linux/noAudio.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-file=$1
-	ffmpeg.exe \
-    -i "$file" \
-	-c copy \
-    -c:v copy \
-    -an \
-    "${file%.*}-nosound.mp4"
-	
